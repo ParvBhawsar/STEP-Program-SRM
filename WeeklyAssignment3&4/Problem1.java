ea8968909ba976d0647d8545f34e1a764fddcee7
ParvBhawsar <bhawsarparv05@gmail.com>
2026-04-02 14:04:08 +0530

    Added Problem1 transaction fee sorting

--- WeeklyAssignment3&4/Problem1.java
+++ WeeklyAssignment3&4/Problem1.java
@@ -1,68 +1,45 @@
 import java.util.*;
 
 class Transaction {
-    String id;
+    String id, ts;
     double fee;
-    String timestamp;
 
-    Transaction(String id, double fee, String timestamp) {
+    Transaction(String id, double fee, String ts) {
         this.id = id;
         this.fee = fee;
-        this.timestamp = timestamp;
+        this.ts = ts;
+    }
+
+    public String toString() {
+        return id + ":" + fee + "@" + ts;
     }
 }
 
 public class Problem1 {
-
-    static void bubbleSort(ArrayList<Transaction> list) {
-        int n = list.size();
-        for (int i = 0; i < n - 1; i++) {
-            boolean swapped = false;
-            for (int j = 0; j < n - i - 1; j++) {
-                if (list.get(j).fee > list.get(j + 1).fee) {
-                    Collections.swap(list, j, j + 1);
-                    swapped = true;
-                }
-            }
-            if (!swapped) break;
-        }
-    }
-
-    static void insertionSort(ArrayList<Transaction> list) {
-        for (int i = 1; i < list.size(); i++) {
-            Transaction key = list.get(i);
-            int j = i - 1;
-
-            while (j >= 0 && list.get(j).fee > key.fee) {
-                list.set(j + 1, list.get(j));
-                j--;
-            }
-            list.set(j + 1, key);
-        }
-    }
-
     public static void main(String[] args) {
         ArrayList<Transaction> list = new ArrayList<>();
-
         list.add(new Transaction("id1", 10.5, "10:00"));
         list.add(new Transaction("id2", 25.0, "09:30"));
         list.add(new Transaction("id3", 5.0, "10:15"));
 
-        bubbleSort(list);
+        // Bubble Sort by fee
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = 0; j < list.size() - i - 1; j++) {
+                if (list.get(j).fee > list.get(j + 1).fee) {
+                    Transaction temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
+                }
+            }
+        }
 
-        System.out.println("Bubble Sort:");
-        for (Transaction t : list)
-            System.out.println(t.id + " " + t.fee);
+        System.out.println("Bubble Sort: " + list);
 
-        insertionSort(list);
-
-        System.out.println("\nInsertion Sort:");
-        for (Transaction t : list)
-            System.out.println(t.id + " " + t.fee);
-
-        System.out.println("\nHigh Fee Outliers:");
-        for (Transaction t : list)
+        // High fee outliers
+        System.out.print("High-fee outliers: ");
+        for (Transaction t : list) {
             if (t.fee > 50)
-                System.out.println(t.id);
+                System.out.print(t + " ");
+        }
     }
 }
