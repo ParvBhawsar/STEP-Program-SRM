2ececdbe59551bedfb044937061f946e2c8579cf
ParvBhawsar <bhawsarparv05@gmail.com>
2026-04-02 14:00:26 +0530

    Added Problem4 portfolio return sorting

---
+++ WeeklyAssignment3&4/Problem4.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class Problem4 {
+    static void quickSort(double[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    static int partition(double[] arr, int low, int high) {
+        double pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] > pivot) { // Descending
+                i++;
+                double temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        double temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        double[] returns = {12, 8, 15};
+        quickSort(returns, 0, returns.length - 1);
+        System.out.println("Quick Desc: " + Arrays.toString(returns));
+    }
+}
